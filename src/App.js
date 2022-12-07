c8c63505b90729301f1a950ec92b17e0c26c7f8a
shanicesauce <shanicesauceda@gmail.com>
2022-12-06 20:10:39 -0600

    slight change in attempt to render page

--- src/App.js
+++ src/App.js
@@ -7,29 +7,27 @@ import ContactForm from "./components/Contact";
 import Footer from "./components/Footer";
 import Resume from "./components/Resume";
 import Welcome from "./components/Welcome";
-import { BrowserRouter as Router, Route, HashRouter } from "react-router-dom";
+import { HashRouter as Router, Route } from "react-router-dom";
 import "./App.css";
 
 function App() {
   const [contactSelected, setContactSelected] = useState(false);
 
   return (
-    <HashRouter basename="/#">
       <div>
         <Nav
           contactSelected={contactSelected}
           setContactSelected={setContactSelected}
         ></Nav>
         <Router>
-          <Route exact path="/#/" element={Welcome} />
-          <Route exact path="/#/about" element={About} />
-          <Route exact path="/#/portfolio" element={Portfolio} />
-          <Route exact path="/#/contact" element={ContactForm} />
-          <Route exact path="/#/resume" element={Resume} />
+          <Route exact path="/" component={Welcome} />
+          <Route exact path="/about" component={About} />
+          <Route exact path="/portfolio" component={Portfolio} />
+          <Route exact path="/contact" component={ContactForm} />
+          <Route exact path="/resume" component={Resume} />
         </Router>
         <Footer />
       </div>
-    </HashRouter>
   );
 }
 

0ad2ea2f0500ef2b45a460f87e6a21604567e153
Shanice Sauceda <shanicesauceda@gmail.com>
2023-01-18 21:44:11 -0600

    updated character size

--- src/components/Welcome/index.js
+++ src/components/Welcome/index.js
@@ -4,7 +4,7 @@ import bear from "../../assets/images/bear.webp";
 const Welcome = () => {
   return (
     <div id="welcome">
-      <img src={bear} alt="cute bear" height="450px" width="400px" className="container"></img>
+      <img src={bear} alt="cute bear" height="722px" width="500px" className="container"></img>
       <p>
         <h1>Memento Mori</h1>
       </p>
